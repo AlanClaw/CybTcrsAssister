30c80bb0d07c9913922653b54efa7c6d3e3ef3fd
alanliu <alanliu@cybersoft4u.com>
2018-04-30 10:25:02 +0800

    remove no use library

--- PageObject/PageTcrs.py
+++ PageObject/PageTcrs.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
 from loc import TcrsControl as loc

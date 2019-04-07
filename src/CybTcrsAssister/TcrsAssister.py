771bae4a71ca1c2aca53453264921a58f521d466
Alan Liu (劉承易) <alanliu@cybersoft4u.com>
2019-04-07 14:38:55 +0800

    remove logger

--- src/CybTcrsAssister/TcrsAssister.py
+++ src/CybTcrsAssister/TcrsAssister.py
@@ -3,15 +3,7 @@
 import getpass
 
 from CybTcrsAssister.Control.TcrsAgent import Agent
-from CybTcrsAssister.utils.LoggingHelper import get_logger
 
-logger = get_logger('./logging.ini')
-
-# logging.basicConfig(
-#     format='[%(asctime)s] %(levelname)s [%(funcName)s] - %(message)s [%(filename)s:%(lineno)d]',
-#     level=logging.DEBUG,
-#     datefmt='%y-%m-%d %H:%M:%S'
-# )
 
 office_pwd = getpass.getpass("Please input your domain password:")
 
